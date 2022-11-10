734f79d3c08129648481e8146df5bc7d192d5256
heejeong <hjx66@naver.com>
2022-11-10 11:05:20 +0900

    initialize project

---
+++ src/main/java/com/bbs/springbootbbsreview/SpringbootBbsReviewApplication.java
@@ -0,0 +1,13 @@
+package com.bbs.springbootbbsreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBbsReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBbsReviewApplication.class, args);
+    }
+
+}
